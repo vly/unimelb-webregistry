a4dea7461a2b8b418313b43739128684e3fd508e
nikvi <nikki.vinayan@gmail.com>
2014-07-24 22:25:07 +1000

    removed duplicate file, formatted app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -1,26 +1,10 @@
 require 'sinatra'
-
-require_relative 'dataEndPoints.rb'
-
+require_relative './dataEndPoints.rb'
 
 class AppEndPoints < Sinatra::Base
-
-    
-
-
-	get '/index' do 
-		"Hello World"
-	end
-
-
 	get '/search/:query' do
 		@search = DataEndPoints.new
 		value = @search.get_data(params[:query])
 		return value
     end
-
-
-
-
-
 end
