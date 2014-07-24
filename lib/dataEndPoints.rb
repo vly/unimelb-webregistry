f368801b29126fb48e2bcd5601a55ef131309cff
nikvi <nikki.vinayan@gmail.com>
2014-07-24 22:48:35 +1000

    batch queries

--- lib/dataEndPoints.rb
+++ lib/dataEndPoints.rb
@@ -17,7 +17,7 @@ class DataEndPoints
 	def getDepartment(pFaculty)
         #facultyName : faculty + websites
         pVal = "\".*"+pFaculty+".*\"" 
-        @neo.execute_query("match (y:Faculty)-[r]->(x:Website) where y.name=~#{pVal}  return x,y,r")
+        return :execute_query, "match (y:Faculty)-[r]->(x:Website) where y.name=~#{pVal}  return x,y,r"
 	end
 
 
@@ -25,7 +25,7 @@ class DataEndPoints
         # gets all data associated with the person
         #persons name : websites + person
         pVal = "\".*"+pName+".*\""
-        @neo.execute_query(" MATCH (x:Website)-[r]->(y:Person) where y.name=~#{pVal} RETURN x,y,r")
+         return :execute_query, "match (x:Website)-[r]->(y:Person) where y.name=~#{pVal} return x,y,r"
     end
 
 
@@ -35,28 +35,12 @@ class DataEndPoints
         #url : website + department +person+relationship
         pVal = "\".*"+pName+".*\""
         #match (y:Faculty)-[:Owns]->(x:Website) where x.url=~'.*melb.*' return y
-        @neo.execute_query("match (x:Website)-[r]-(y) where x.url=~#{pVal} return x,y,r")
+        return :execute_query, "match (x:Website)-[r]-(y) where x.url=~#{pVal} return x,y,r"
     end
 
 
     def get_data(query_string)
-        url_block= getURL(query_string)
-        p_block = getPerson(query_string)
-        dept_block = getDepartment(query_string)
-        op = Hash.new
-        if(!(url_block["data"].empty?))
-            op.merge(url_block)
-            puts "#{op}"
-            return parseData(url_block)
-        end
-        if(!(p_block["data"].empty?))
-              op.merge(p_block)
-        return parseData(p_block)
-        end
-        if(!dept_block["data"].empty?)
-            op.merge(dept_block)
-            return parseData(dept_block)
-        end
+        return @neo.batch getURL(query_string), getPerson(query_string), getDepartment(query_string)
     end
 
 
